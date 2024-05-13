f08e767c91980376d2efb6df42e84c9aebdb0107
Liene <lienebalina@gmail.com>
2024-05-13 20:44:15 +0300

    playtime and gemfile fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
+  validates :title, length: { minimum:10, message: "Must be at least 10 characters long!"}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
