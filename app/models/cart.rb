ba32110f1d2fadc69b373c346c6cf20a1bbd2164
Liene <lienebalina@gmail.com>
2024-05-28 15:12:10 +0300

    fixed cart model tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,16 +5,28 @@ class Cart < ApplicationRecord
     current_item = line_items.find_by(product_id: product.id)
     if current_item
       current_item.quantity += 1
+      current_item.save
     else
       current_item = line_items.build(
         product_id: product.id,
         price: product.price
       )
+      current_item.save
     end
     current_item
   end
 
   def total_price
     line_items.sum { |item| item.total_price }
+  end
+
+  # counts the total quantity of line items
+  def item_count
+    line_items.sum { |item| item.quantity }
+  end
+
+  # counts how many unique products are in the cart
+  def unique_line_items_count
+    line_items.distinct.count(:product_id)
   end
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "adding unique products to the cart" do
+    cart = Cart.create
+    product1 = products(:ruby)
+    product2 = products(:two)
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    assert_equal 2, cart.unique_line_items_count
+  end
+
+  test "adding duplicate products to the cart" do
+    cart = Cart.create
+    product = products(:ruby)
+
+    cart.add_product(product)
+    cart.add_product(product)
+
+    assert_equal 1, cart.unique_line_items_count
+    assert_equal 2, cart.item_count
+  end
 end
