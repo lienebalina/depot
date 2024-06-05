c6abc0070af27eb9d813bf978d4fd5b0de4dab7f
Liene <lienebalina@gmail.com>
2024-06-05 19:48:28 +0300

    added stimulus

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,8 +2,9 @@
 
 # Line item model class
 class LineItem < ApplicationRecord
+  belongs_to :order, optional: true
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
 
   def total_price
     product.price * quantity
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,7 @@
 
 pin 'application'
 pin '@rails/actioncable', to: 'actioncable.esm.js'
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from 'app/javascript/channels', under: 'channels'
